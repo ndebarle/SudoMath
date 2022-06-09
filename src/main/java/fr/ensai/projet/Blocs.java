ff6c15a0efb7dc59cd9709ac66bbb53bf9b28502
evainmanon <evain.manon@orange.fr>
2022-06-09 11:10:46 +0200

    Classe bloc et exercices

---
+++ src/main/java/fr/ensai/projet/Blocs.java
@@ -0,0 +1,9 @@
+package fr.ensai.projet;
+
+public Class Blocs {
+    public String[] enonce;
+    public String[][] variable;
+    public int[][] reponses;
+    public boolean[] isVariable;
+    public int[][] lienSolutionVariable;
+}
