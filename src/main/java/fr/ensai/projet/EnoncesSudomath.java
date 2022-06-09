4ae0ffe17c9f5893eeea18ed0bcc9f90c95a830e
evainmanon <evain.manon@orange.fr>
2022-06-09 12:18:14 +0200

    Changement nom classe

--- src/main/java/fr/ensai/projet/EnoncesSudomath.java
+++ src/main/java/fr/ensai/projet/EnoncesSudomath.java
@@ -1,12 +1,12 @@
 package fr.ensai.projet;
 
-public class SujetSudomath {
+public class EnoncesSudomath {
     public int  id;
     public String commentaires;
     public Exercices[] exercices;
     public Utilisateur auteur; 
 
-    public SujetSudomath (int id, String commentaires, Exercices[] exercices, Utilisateur auteur){
+    public EnoncesSudomath (int id, String commentaires, Exercices[] exercices, Utilisateur auteur){
         this.id = id;
         this.commentaires = commentaires;
         this.exercices = exercices;
---
+++ src/main/java/fr/ensai/projet/SujetSudoMaths.java
@@ -0,0 +1,12 @@
+package fr.ensai.projet;
+
+public class SujetSudoMaths {
+    public Sudoku[] grille;
+    public EnoncesSudomath sujet; 
+    public Utilisateur proprietaire;
+    public ModeleLatex modele;
+
+    public SujetSudoMaths (Sudoku[] grille, SujetSudomath sujet, Utilisateur proprietaire) {
+        
+    }
+}
