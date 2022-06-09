4f5a0925509dfea2738fc49463420373ecf7a770
evainmanon <evain.manon@orange.fr>
2022-06-09 11:27:24 +0200

    Classe Exemplaire Sudomath

---
+++ src/main/java/fr/ensai/projet/ExemplaireSudoMaths.java
@@ -0,0 +1,11 @@
+package fr.ensai.projet;
+
+public class ExemplaireSudoMaths {
+    public Sudoku[] grille;
+    public SujetSudomath sujet; 
+    public Utilisateur proprietaire;
+
+    public ExemplaireSudoMaths (Sudoku[] grille, SujetSudomath sujet, Utilisateur proprietaire) {
+        
+    }
+}
